75687c80277495c1cd411226895b2273b9c7f00e
osadhi <osadhiv@gmail.com>
2024-01-30 23:20:35 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,17 +1,23 @@
 import argparse
 import logging
+import os
+import sys
 from configparser import ConfigParser
 from logging.handlers import RotatingFileHandler
 
 from auto_seedr import AutoSeedrClient, setup_config
 
+DEFAULT_CONFIG_FILE = 'config.ini'
+DEFAULT_LOG_LEVEL = 'ERROR'
+DEFAULT_LOG_FILE = 'auto_seedr_cli.log'
+
 
 def configure_logging(log_level: str, log_file: str) -> None:
     """
-    Configure logging based on the specified log level.
+    Configure logging for the application.
 
-    :param log_level: The log level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    :param log_file: The log file path
+    :param log_level: The desired logging level.
+    :param log_file: The file path for the log file.
     :return: None
     """
     logging.basicConfig(level=log_level.upper())
@@ -28,40 +34,102 @@ def configure_logging(log_level: str, log_file: str) -> None:
     logging.getLogger().addHandler(console_handler)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='AutoSeedr CLI')
-    parser.add_argument('-c', '--create-config', action='store_true', help='Create a new config file')
-    parser.add_argument('-f', '--config-file', default='config.ini', help='INI file path for configuration')
-    parser.add_argument('-e', '--email', help='Seedr account email')
-    parser.add_argument('-p', '--password', help='Seedr account password')
-    parser.add_argument('-td', '--torrent-directory', default='torrents', help='Directory containing torrent files')
-    parser.add_argument('-dd', '--download-directory', default='downloads', help='Directory to store downloaded files')
-    parser.add_argument('-cs', '--chunk-size', default='1024', help='Chunk size for downloading files')
-    parser.add_argument('-l', '--log-level', default='ERROR',
-                        help='Logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)')
-    parser.add_argument('-lf', '--log-file', default='auto_seedr_cli.log', help='Log file path')
+def get_user_input(prompt, default=None):
+    """
+    Get user input with an optional default value.
 
-    return parser.parse_args()
+    :param prompt: The prompt to display to the user.
+    :param default: The default value if the user input is empty.
+    :return: User input or the default value.
+    """
+    user_input = input(prompt)
+    return user_input.strip() if user_input.strip() != '' else default
+
+
+def parse_args():
+    """
+    Parse command-line arguments using argparse.
+
+    :return: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description='AutoSeedr CLI')
+
+    parser.add_argument('-C', '--create-config', action='store_true', help='Create a new config file')
+    parser.add_argument('-L', '--load-config', action='store_true', help='Load an existing config file')
+
+    config_group = parser.add_argument_group('Configuration Options')
+    config_group.add_argument('-f', '--config-file', default=DEFAULT_CONFIG_FILE,
+                              help='INI file path for configuration (default: config.ini)')
+    config_group.add_argument('-e', '--email', help='Seedr account email')
+    config_group.add_argument('-p', '--password', help='Seedr account password')
+    config_group.add_argument('-td', '--torrent-directory', default='torrents',
+                              help='Directory containing torrent files (default: torrents)')
+    config_group.add_argument('-dd', '--download-directory', default='downloads',
+                              help='Directory to store downloaded files (default: downloads)')
+    config_group.add_argument('-cs', '--chunk-size', default=1024, type=int,
+                              help='Chunk size for downloading files in kilobytes (default: 1024)')
+
+    log_group = parser.add_argument_group('Logging Options')
+    log_group.add_argument('-ll', '--log-level', default=DEFAULT_LOG_LEVEL,
+                           choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                           help='Logging level (default: ERROR)')
+    log_group.add_argument('-lf', '--log-file', default=DEFAULT_LOG_FILE,
+                           help='Log file path (default: auto_seedr_cli.log)')
+
+    args = parser.parse_args()
+
+    if not args.create_config and not args.load_config and not (args.email and args.password):
+        parser.print_help()
+        print(
+            "\nInvalid combination of arguments. Please provide either --create-config, --load-config, or valid email "
+            "and password.")
+        sys.exit(1)
+
+    return args
+
+
+def prompt_for_missing_details():
+    """
+    Prompt the user for missing details (email and password).
+
+    :return: Tuple containing email and password.
+    """
+    email = get_user_input('Seedr account email: ')
+    password = get_user_input('Seedr account password: ')
+    return email, password
 
 
 def main():
+    """
+    The main function that orchestrates the AutoSeedr CLI functionality.
+    """
     args = parse_args()
-
-    configure_logging(args.log_level, args.log_file)
+    torrent_directory, download_directory, chunk_size = (
+        args.torrent_directory,
+        args.download_directory,
+        args.chunk_size
+    )
 
     if args.create_config:
+        if not args.email or not args.password:
+            logging.warning('Email and password are required to create a new config.')
+            email, password = prompt_for_missing_details()
+        else:
+            email, password = args.email, args.password
+
         setup_config(
-            email=args.email,
-            password=args.password,
+            email=email,
+            password=password,
             config_file=args.config_file,
             torrent_directory=args.torrent_directory,
             download_directory=args.download_directory,
             chunk_size=args.chunk_size
         )
         logging.info(f'Config file "{args.config_file}" created successfully.')
-        return
-
-    if args.config_file:
+    elif args.load_config:
+        if not os.path.exists(args.config_file):
+            logging.error(f'Config file "{args.config_file}" not found.')
+            return
         config = ConfigParser()
         config.read(args.config_file)
         email = config.get('SEEDR', 'user')
@@ -70,11 +138,15 @@ def main():
         download_directory = config.get('APP', 'download_folder')
         chunk_size = config.get('APP', 'chunk_size')
     else:
-        email = args.email
-        password = args.password
-        torrent_directory = args.torrent_directory
-        download_directory = args.download_directory
-        chunk_size = args.chunk_size
+        email, password, torrent_directory, download_directory, chunk_size = (
+            args.email,
+            args.password,
+            args.torrent_directory,
+            args.download_directory,
+            args.chunk_size
+        )
+
+    configure_logging(args.log_level, args.log_file)
 
     seedr_client = AutoSeedrClient(
         email=email,
@@ -82,14 +154,13 @@ def main():
         torrent_directory=torrent_directory,
         download_directory=download_directory,
         chunk_size=chunk_size,
-        log_level=args.log_level.upper()
     )
 
     try:
         seedr_client.directory_download()
         logging.info(f'Directory download completed successfully. Files downloaded to: {download_directory}')
     except Exception as e:
-        logging.error(f'Error: {e}')
+        logging.error(f'Error: {e}', exc_info=True)
 
 
 if __name__ == '__main__':
